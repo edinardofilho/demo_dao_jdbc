4aba7bb8a9d4925d914f05abd15ec2e8695eb155
Edinardo Freitas Filho <edinardofreitas@gmail.com>
2020-08-09 17:05:22 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,8 +13,9 @@ public class Program {
 		Department obj = new Department(1, "Books");
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		Seller seller = sellerDao.findById(3);
+		
 		System.out.println(seller);
 	}
 }
